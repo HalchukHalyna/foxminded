3f2d3951f025ff4d848f33462e9960ee318951be
HalchukHalyna <halchuk.halyna@gmail.com>
2017-04-02 22:43:52 +0300

    Add files via upload
    Was added possibility to run cross-browser testing one by one.

--- Foxminded/src/test/java/ua/com/foxminded/BaseTest.java
+++ Foxminded/src/test/java/ua/com/foxminded/BaseTest.java
@@ -1,70 +1,51 @@
 package ua.com.foxminded;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
 import java.util.concurrent.TimeUnit;
+
 import static org.testng.Assert.fail;
 
-/**
- * Created by Gerti on 02.04.2017.
- */
 public class BaseTest {
     protected WebDriver driver;
     protected String baseUrl;
     private boolean acceptNextAlert = true;
     private StringBuffer verificationErrors = new StringBuffer();
 
+    private static final String CHROME_BROWSER = "chrome";
+    private static final String FIREFOX_BROWSER = "firefox";
+
+
     @BeforeClass(alwaysRun = true)
-    public void setUp() throws Exception {
-//       System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-//       driver = new ChromeDriver();
-        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
-       driver = new FirefoxDriver();
-       baseUrl = "https://www.google.com.ua/";
-       driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    @Parameters("browser")
+    public void setUp(String browser) throws Exception {
+        switch (browser) {
+            case CHROME_BROWSER:
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+            case FIREFOX_BROWSER:
+                System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+            default:
+                System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+        }
+
+        baseUrl = "https://www.google.com.ua/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @AfterClass(alwaysRun = true)
     public void tearDown() throws Exception {
         driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
     }
 }
+
--- Foxminded/src/test/java/ua/com/foxminded/Foxminded.java
+++ Foxminded/src/test/java/ua/com/foxminded/Foxminded.java
@@ -5,9 +5,6 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-/**
- * Created by Gerti on 01.04.2017.
- */
 public class Foxminded extends BaseTest{
 
     @Test
@@ -16,10 +13,8 @@ public class Foxminded extends BaseTest{
         driver.findElement(By.id("lst-ib")).clear();
         driver.findElement(By.id("lst-ib")).sendKeys("foxminded");
         driver.findElement(By.id("_fZl")).click();
-        //driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/h3/a")).click(); //to click at the first link from search result
         driver.findElement(By.linkText("Foxminded")).click();
         assertTrue(driver.findElement(By.cssSelector("img[alt=\"foxminded logo\"]")).isDisplayed());
         assertEquals("Главная", driver.getTitle());
-        System.out.printf(driver.getTitle());
     }
 }
